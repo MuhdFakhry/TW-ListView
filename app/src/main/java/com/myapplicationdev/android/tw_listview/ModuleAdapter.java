540c1348d14daf99bd832321782f5c87a1d71137
17010578 <shijie_00@hotmail.com>
2019-04-30 14:33:32 +0800

    SecondActivity

--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -1,12 +1,67 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ModuleAdapter {
+import java.util.ArrayList;
 
+public class ModuleAdapter extends ArrayAdapter<Module> {
+
+    private ArrayList<Module> module;
     private Context context;
+    private TextView tvModName;
+    private ImageView ivMod;
+
+    public ModuleAdapter(Context context, int resource, ArrayList<Module> objects){
+        super(context, resource, objects);
+        // Store the food that is passed to this adapter
+        module = objects;
+        // Store Context object as we would need to use it later
+        this.context = context;
+    }
+
+    // getView() is the method ListView will call to get the
+    //  View object every time ListView needs a row
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // The usual way to get the LayoutInflater object to
+        //  "inflate" the XML file into a View object
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // "Inflate" the row.xml as the layout for the View object
+        View rowView = inflater.inflate(R.layout.module, parent, false);
+
+        // Get the TextView object
+        tvModName = rowView.findViewById(R.id.tvModName);
+        // Get the ImageView object
+        ivMod = rowView.findViewById(R.id.ivMod);
+
+
+        // The parameter "position" is the index of the
+        //  row ListView is requesting.
+        //  We get back the food at the same index.
+        Module currentMod = module.get(position);
+        // Set the TextView to show the food
+
+        tvModName.setText(currentMod.getName());
+        // Set the image to star or nostar accordingly
+        if(currentMod.isMod()) {
+            ivMod.setImageResource(R.drawable.prog);
+
+        }
+        else {
+            ivMod.setImageResource(R.drawable.nonprog);
+        }
+        // Return the nicely done up View to the ListView
+        return rowView;
+    }
 
 
 
 
 }
+
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,21 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
+    ArrayAdapter aa;
+    ArrayList<Module> module ;
 
 
     @Override
@@ -24,10 +32,29 @@ public class SecondActivity extends AppCompatActivity {
         String year = i.getStringExtra("year");
         tvYear.setText(year);
 
+        module = new ArrayList<Module>();
+        module.add(new Module("C208", true));
+        module.add(new Module("C200", false));
+        module.add(new Module("C346", true));
+
+        aa = new ModuleAdapter(this,R.layout.module,module);
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Module selectedFood = module.get(position);
+
+                Toast.makeText(SecondActivity.this, selectedFood.getName()
+                                + " Star: " + selectedFood.isMod(),
+                        Toast.LENGTH_LONG).show();
+
+            };
+        });
 
 
-    }
+    };
 
 
 
-}
+};
